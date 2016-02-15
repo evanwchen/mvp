3b90846f2b8e1e07f7005989bbe6cb34e2ef4e27
Evan Chen <evanwchen@gmail.com>
2016-02-15 11:50:39 -0800

    Add bower.json and package.json. Set up Node, Express, and Angular.

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+var port = 8000;
+app.listen(port);
+console.log('Server now listening on port ' + port);
+
+module.exports = app;
