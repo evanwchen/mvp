dc0d3ee81f18210ca8082ebcad44eba7f41d1b2f
Evan Chen <evanwchen@gmail.com>
2016-02-16 15:10:18 -0800

    Enable Remove from List button

--- client/app/main/list/list.js
+++ client/app/main/list/list.js
@@ -7,5 +7,12 @@ angular.module('dayplanner.list', [])
     console.log('$scope.venues: ', $scope.venues);
   });
 
+  $scope.removeFromList = function(index) {
+    console.log('index:', index);
+    console.log('$scope.venues b4:', $scope.venues);
+    $scope.venues.splice(index, 1);
+    console.log('$scope.venues after:', $scope.venues);
+  };
+
 });
 
